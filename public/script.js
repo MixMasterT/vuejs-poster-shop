3acf098955bee4a17151fcbe661871728515cfd6
MixMasterT <torahlight@gmail.com>
2018-07-17 13:24:37 -0500

    Update logic for cart management

--- public/script.js
+++ public/script.js
@@ -3,17 +3,60 @@ new Vue({
   data: {
     total: 0,
     items: [
-      {title: 'Item One'},
-      {title: 'Item Two'},
-      {title: 'Item Three'},
+      {id: 1, title: 'Item One', price: 9.99},
+      {id: 2, title: 'Item Two', price: 7.99},
+      {id: 3, title: 'Item Three', price: 12.99},
     ],
     cart: [],
   },
   methods: {
     addItem(idx) {
-      this.cart.push(this.items[idx]);
-      console.log("cart: " + this.cart);
+      var item = this.items[idx];
+      var found = false;
+      for(var i = 0; i < this.cart.length; i++) {
+        if(this.cart[i].id === item.id) {
+          this.cart[i].qty++;
+          found = true;
+        }
+      }
+      if(!found) {
+        this.cart.push({
+          id: item.id,
+          title: item.title,
+          price: item.price,
+          qty: 1,
+        });
+      }
       this.total += 10;
+    },
+    getTotal() {
+      var total = 0;
+      for(var j = 0; j < this.cart.length; j++) {
+        var item = this.cart[j];
+        total += item.price * item.qty;
+      }
+      return total;
+    },
+    inc(item) {
+      item.qty++;
+    },
+    dec(item) {
+      if(item.qty > 0) {
+        item.qty--;
+      }
+      if(item.qty === 0) {
+        for(var k = 0; k < this.cart.length; k++) {
+          if(this.cart[k].id === item.id) {
+            this.cart.splice(k, 1);
+            break;
+          }
+        }
+      }
+    }
+  },
+  filters: {
+    currency: function(price) {
+      return `$${price.toFixed(2)}`;
     }
   }
 });
