88f584d4fbbb893d871276639f4320ece8eace11
Torah Oglander <torahoglander@xs-MacBook-Pro.local>
2018-07-17 11:47:36 -0500

    Update basic functionality

--- public/script.js
+++ public/script.js
@@ -1,1 +1,19 @@
-console.log('It works.');
+new Vue({
+  el: '#app',
+  data: {
+    total: 0,
+    items: [
+      {title: 'Item One'},
+      {title: 'Item Two'},
+      {title: 'Item Three'},
+    ],
+    cart: [],
+  },
+  methods: {
+    addItem(idx) {
+      this.cart.push(this.items[idx]);
+      console.log("cart: " + this.cart);
+      this.total += 10;
+    }
+  }
+});
